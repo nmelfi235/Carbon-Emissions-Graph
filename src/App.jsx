d4d0ae80ec8abefbe3f25fa45a64238f7b59e9f3
Nick Melfi <nmelfi235@gmail.com>
2025-01-31 11:23:24 -0500

    finished attaching data, only works with no-cors

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,80 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import LineGraph from "./components/LineGraph";
 
+const scaleFactors = {
+  "": 1,
+  Refuse: 0.02638,
+  Wood: 0.01946,
+  Coal: 0.01733,
+  Oil: 0.01478,
+  "Landfill Gas": 0.01327,
+  "Natural Gas": 0.00656,
+};
+
 const fetchData = async (username, pswd) => {
-  console.log(username, pswd);
   const url =
     "https://webservices.iso-ne.com/api/v1.1/genfuelmix/day/20250129.json";
   const headers = new Headers();
   headers.append("Authorization", `Basic ${btoa(`${username}:${pswd}`)}`);
 
   return await fetch(url, {
-    mode: "cors",
-    method: "GET",
     headers: headers,
-  });
+  }).then((res) => res.json());
 };
 
 function App() {
-  const [data, setData] = useState([]); // Data will be an array of objects
+  const [data, setData] = useState([
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+    [{ Date: "0", FuelCategory: "Refuse", Emissions: "0" }],
+  ]); // Data will be an array of objects
   const [username, setUsername] = useState("");
   const [pswd, setPswd] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchData(username, pswd).then((d) => console.log(d));
+    fetchData(username, pswd)
+      .then((d) => d["GenFuelMixes"]["GenFuelMix"])
+      .then((d) =>
+        d.map((datum) => {
+          return {
+            Date: datum["BeginDate"],
+            FuelCategory: datum["FuelCategory"],
+            Emissions: (
+              datum["GenMw"] * scaleFactors[datum["FuelCategory"]]
+            ).toFixed(2),
+          };
+        })
+      )
+      .then((d) => [
+        d.filter((datum) => datum["FuelCategory"] === "Refuse"),
+        d.filter((datum) => datum["FuelCategory"] === "Wood"),
+        d.filter((datum) => datum["FuelCategory"] === "Coal"),
+        d.filter((datum) => datum["FuelCategory"] === "Oil"),
+        d.filter((datum) => datum["FuelCategory"] === "Landfill Gas"),
+        d.filter((datum) => datum["FuelCategory"] === "Natural Gas"),
+      ])
+      .then((d) => {
+        const totalArr = [];
+        for (let i = 0; i < d[0].length; i++) {
+          let sum = 0;
+          for (let j = 0; j < d.length; j++) {
+            sum += parseFloat(d[j][i]["Emissions"]);
+          }
+          totalArr.push({
+            Date: d[0][i]["Date"],
+            FuelCategory: "Total",
+            Emissions: sum,
+          });
+        }
+        d.push(totalArr);
+        return d;
+      })
+      .then((d) => setData(d))
+      .then((d) => console.log(data));
   };
 
   return (
@@ -43,7 +95,7 @@ function App() {
       <button type="button" onClick={handleSubmit}>
         Submit
       </button>
-      <LineGraph data={data} />
+      <LineGraph data={data} height={800} />
     </>
   );
 }
--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 
 // Data must be pre-processed before being input into LineGraph
@@ -7,26 +7,147 @@ export default function LineGraph({
   marginRight = 50,
   marginBottom = 50,
   marginLeft = 50,
-  width = 1000,
-  height = 1000,
+  width = 800,
+  height = 400,
   data,
 }) {
+  const [d, setD] = useState(data);
+  /*
+  const tooltip = useRef(d3.create("g"));
+  const bisect = d3.bisector((d) => new Date(d["Date"])).center; // function that gets the
+  const pointerMoved = (e) => {
+    const i = bisect(data, x.invert(d3.pointer(e)[0]));
+
+    void d3
+      .select(tooltip.current)
+      .style("display", null)
+      .attr(
+        "transform",
+        `translate(${d3.pointer(e)[0]},${d3.pointer(e)[1] + 15})`
+      );
+
+    const path = d3
+      .select(tooltip.current)
+      .selectAll("path")
+      .data([,])
+      .join("path")
+      .attr("fill", "white")
+      .attr("stroke", "black");
+
+    const text = d3
+      .select(tooltip.current)
+      .selectAll("text")
+      .data([,])
+      .join("text")
+      .call((text) =>
+        text
+          .selectAll("tspan")
+          .data(
+            data.map(
+              (d, i) =>
+                `${d[i]["Date"]}:  ${d3.timeFormat("%a %B %d %I:%M %p")(
+                  new Date(data[i][d])
+                )}
+                 ${d[i]["FuelCategory"]}: ${
+                  d3.format(".2f")(parseFloat(d[i]["Emissions"])) +
+                  " metric tons per minute"
+                }`
+            )
+          )
+          .join("tspan")
+          .attr("class", (d) => "tooltip-label " + d.match(/\w+/))
+          .attr("x", 0)
+          .attr("y", (d, i) => `${i * 1.1}em`)
+          .attr("font-weight", (d, i) => (i ? null : "bold"))
+          .text((d) => d)
+      );
+
+    size(text, path);
+  };
+
+  function pointerLeft() {
+    d3.select(tooltip.current).style("display", "none");
+  }
+
+  function size(text, path) {
+    const { x, y, width: w, height: h } = text.node().getBBox();
+    text.attr("transform", `translate(${-w / 2},${15 - y})`);
+    path.attr(
+      "d",
+      `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`
+    );
+  }
+*/
   const svgRef = useRef(d3.create("svg"));
   useEffect(() => {
-    d3.select(svgRef.current);
-  }, [svgRef]);
+    d3.select(svgRef.current)
+      .attr("width", width + marginLeft + marginRight)
+      .attr("height", height + marginBottom + marginTop);
+    //.on("pointerenter pointermove", pointerMoved)
+    //.on("pointerleave", pointerLeft);
+  }, [data, svgRef]);
 
-  const dateData = data.map((datum) => new Date(datum["Date"]));
   const x = d3.scaleTime(
-    [d3.min(dateData), d3.max(dateData)],
+    [
+      new Date(data[0][0]["Date"]),
+      new Date(data[0][data[0].length - 1]["Date"]),
+    ],
     [marginLeft, width]
   );
 
-  const emissionData = data.map((datum) => parseFloat(datum["Emissions"]));
+  const emissionData = data.flat().map((d) => parseFloat(d["Emissions"]));
   const y = d3.scaleLinear(
     [d3.min(emissionData), d3.max(emissionData)],
     [height - marginBottom, marginTop]
   );
 
-  return <svg ref={svgRef} />;
+  const colors = {
+    Refuse: "#723b17",
+    Wood: "#AB4A00",
+    Coal: "#3E4B50",
+    Oil: "#62777F",
+    "Landfill Gas": "#40D397",
+    "Natural Gas": "#6dcff6",
+    Total: "#f63c3d",
+  };
+
+  const xAxisRef = useRef(d3.create("g"));
+  useEffect(() => {
+    d3.select(xAxisRef.current)
+      .attr("transform", `translate(0, ${height - marginBottom})`)
+      .call(d3.axisBottom(x));
+  }, [data, xAxisRef]);
+
+  const yAxisRef = useRef(d3.create("g"));
+  useEffect(() => {
+    d3.select(yAxisRef.current)
+      .attr("transform", `translate(${marginLeft}, 0)`)
+      .call(d3.axisLeft(y));
+  }, [data, yAxisRef]);
+
+  const line = d3
+    .line()
+    .x((d) => x(new Date(d["Date"])))
+    .y((d) => y(parseFloat(d["Emissions"])))
+    .curve(d3.curveNatural);
+
+  const linesRef = useRef(d3.create("g"));
+  useEffect(() => {
+    d3.select(linesRef.current)
+      .selectAll("path")
+      .data(data)
+      .join("path")
+      .attr("d", line)
+      .attr("stroke", (d, i) => colors[d[0]["FuelCategory"]])
+      .attr("fill", "transparent");
+  }, [data, linesRef]);
+
+  return (
+    <svg ref={svgRef}>
+      {/*<g ref={tooltip} />*/}
+      <g ref={xAxisRef} />
+      <g ref={yAxisRef} />
+      <g ref={linesRef} />
+    </svg>
+  );
 }
